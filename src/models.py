bb58a8ad19e3cbc521704022c17deede0e7634d0
Fabio Seel <fabio.seel@gmail.com>
2023-09-26 17:11:03 +0200

    feat: basic implementation of a model + training function

---
+++ src/models.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import yaml
+import math
+
+class SimpleCNN(nn.Module):
+    def __init__(self, img_size, num_classes, num_layers=3, in_channels=3, num_channels=16,
+                 kernel_size=3, stride=1, dilation=1):
+        super(SimpleCNN, self).__init__()
+        self.img_size = img_size
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.in_channels = in_channels
+        self.num_channels = num_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.dilation = dilation
+        
+        # Define the first convolutional layer
+        self.conv1 = nn.Conv2d(in_channels, num_channels, kernel_size, stride, dilation=dilation)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=-1)
+        
+        # Define additional convolutional layers if needed
+        self.extra_conv_layers = nn.ModuleList()
+        for _ in range(num_layers - 1):
+            self.extra_conv_layers.append(nn.Conv2d(num_channels, num_channels, kernel_size, stride, dilation=dilation))
+        
+        # Fully connected layer
+        res_size = self.img_size
+        for l in range(self.num_layers):
+            res_size = math.floor((res_size-dilation*(kernel_size-1)-1)/stride + 1)
+        self.fc = nn.Linear(num_channels * res_size**2, num_classes)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        
+        for conv_layer in self.extra_conv_layers:
+            x = conv_layer(x)
+            x = self.relu(x)
+        
+        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = self.fc(x)
+        # if not self.train:
+        #     x = self.softmax(x)
+        return x
+    
+    def save(self, filename):
+        config = {
+            'img_size': self.img_size,
+            'num_classes': self.num_classes,
+            'num_layers': self.num_layers,
+            'in_channels': self.in_channels,
+            'num_channels':self.num_channels,
+            'kernel_size': self.kernel_size,
+            'stride': self.stride,
+            'dilation': self.dilation
+        }
+        with open(filename+".cfg", 'w') as f:
+            yaml.dump(config, f)
+        torch.save(self.state_dict,filename+".pth")
+
+    @classmethod
+    def load(cls, filename):
+        with open(filename, 'r') as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+        model = cls(**config)
+        return model.load_state_dict(torch.load(filename+".pth"))
---
+++ src/training.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+def train(model: nn.Module, optimizer: optim.Optimizer, train_loader: DataLoader):
+    # Define the loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    
+    # Training loop
+    model.train()  # Set the model to training mode
+    running_loss = 0.0
+    
+    epoch_correct = 0
+    for i, (inputs, labels) in enumerate(train_loader):
+        optimizer.zero_grad()  # Zero the parameter gradients
+        
+        # Forward pass
+        outputs = model(inputs)
+        epoch_correct += num_correct(outputs, labels)
+        loss = criterion(outputs, labels)
+        
+        # Backpropagation and optimization
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        
+    # Print training loss for this epoch
+    print(f'Training Loss: {running_loss / len(train_loader)}, Avg. Accuracy: {epoch_correct / len(train_loader.dataset)}')
+
+
+def validate(model: nn.Module, dataloader: DataLoader):
+    # Evaluate the model on the test data
+    model.eval()  # Set the model to evaluation mode
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            outputs = model(inputs)
+            total += labels.size(0)
+            correct += num_correct(outputs, labels)
+    accuracy = 100 * correct / total
+    print(f'Test Accuracy: {accuracy}%')
+    return accuracy
+
+def num_correct(outputs, labels):
+    _, predicted = torch.max(outputs.data, 1)
+    return (predicted == labels).sum().item()
