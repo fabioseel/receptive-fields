fix: model inference behaviour, vis normalization

src/activation_visualization.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 import torch
 import numpy as np
 
+def normalizeZeroOne(input):
+    return (input - input.min()) / (input.max()-input.min())
+
 def dataset_average(model: nn.Module, dataloader: DataLoader, desired_output = torch.Tensor):
     model.eval()  # Set the model to evaluation mode
-    total_weight = 0.0
     weighted_sum = torch.zeros(next(iter(dataloader))[0].shape[1:])
 
     with torch.no_grad():
@@ -17,9 +19,8 @@ def dataset_average(model: nn.Module, dataloader: DataLoader, desired_output = t
             weight = 1.0 / (1.0 + error)
             weighted_inputs = inputs * weight
             weighted_sum += weighted_inputs.sum(dim=0)
-            total_weight += weight.sum(dim=0)
 
-    return weighted_sum / total_weight
+    return normalizeZeroOne(weighted_sum)
 
 def effective_receptive_field(model:nn.Module, output_signal:torch.tensor, input_size:torch.Size, n_iter:int=2048):
     model.eval()
@@ -28,4 +29,4 @@ def effective_receptive_field(model:nn.Module, output_signal:torch.tensor, input
     target_output = output * output_signal
     target_output.sum().backward()
     eff_rf = input_tensor.grad.sum(0)
-    return (eff_rf - eff_rf.min()) / (eff_rf.max()-eff_rf.min())
+    return normalizeZeroOne(eff_rf)

src/models.py
@@ -42,7 +42,7 @@ class SimpleCNN(nn.Module):
         
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = self.fc(x)
-        if not self.train:
+        if not self.training:
             x = self.softmax(x)
         return x
     