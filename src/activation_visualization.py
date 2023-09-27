299335533bb94c478e124f2d11cf6181c054d847
Fabio Seel <fabio.seel@gmail.com>
2023-09-27 16:07:07 +0200

    feat: implement maximization by backpropagation

--- src/activation_visualization.py
+++ src/activation_visualization.py
@@ -30,3 +30,19 @@ def effective_receptive_field(model:nn.Module, output_signal:torch.tensor, input
     target_output.sum().backward()
     eff_rf = input_tensor.grad.sum(0)
     return normalizeZeroOne(eff_rf)
+
+def backprop_maximization(model:nn.Module, output_signal:torch.tensor, input_size:torch.Size, n_iter:int=2048, batch_size=16, reduction=True):
+    model.eval()
+    criterion = nn.CrossEntropyLoss()
+
+    input_tensor = torch.randn((batch_size,*input_size), requires_grad=True)
+    for i in range(n_iter):
+        input_tensor.requires_grad = True
+        output = model(input_tensor.repeat(1,1,1,1))
+        loss = criterion(output, output_signal.repeat(batch_size,1))
+        loss.backward()
+        input_tensor = normalizeZeroOne(input_tensor.detach()-input_tensor.grad)
+    if reduction:
+        return normalizeZeroOne(input_tensor.mean(0)).detach()
+    else:
+        return input_tensor.detach()
