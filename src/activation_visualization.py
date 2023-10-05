437f93688d8b7686c5fde08f4153bea7f77b6722
Fabio Seel <fabio.seel@gmail.com>
2023-10-05 15:07:06 +0200

    feat: add option to smoothen backprop maximization

--- src/activation_visualization.py
+++ src/activation_visualization.py
@@ -91,24 +91,30 @@ def get_input_output_shape(model: nn.Sequential):
     input_size = (in_channels, in_size, in_size)
     return num_outputs, input_size
 
-def backprop_maximization(model:nn.Module, n_iter:int=2048, batch_size=16, reduction=True):
+def backprop_maximization(model:nn.Module, n_iter:int=2048, batch_size=16, reduction=True, smoothened=False):
     num_outputs, input_size = get_input_output_shape(model)
     results = torch.zeros((num_outputs, *input_size))
     for i in tqdm(range(num_outputs)):
         output_signal = torch.zeros(num_outputs)
         output_signal[i] = 1
-        results[i] = single_backprop_maximization(model, output_signal, input_size, n_iter, batch_size, reduction)
+        results[i] = single_backprop_maximization(model, output_signal, input_size, n_iter, batch_size, reduction, smoothened)
     return results
 
-def single_backprop_maximization(model:nn.Module, output_signal:torch.tensor, input_size: torch.Size, n_iter:int=2048, batch_size=16, reduction=True):
+def single_backprop_maximization(model:nn.Module, output_signal:torch.tensor, input_size: torch.Size, n_iter:int=2048, batch_size=16, reduction=True, smoothened=False):
     model.eval()
     criterion = nn.CrossEntropyLoss()
+    smooth_loss = nn.MSELoss()
 
     input_tensor = torch.randn((batch_size,*input_size), requires_grad=True)
     for i in tqdm(range(n_iter)):
         input_tensor.requires_grad = True
         output = model(input_tensor.repeat(1,1,1,1))
         loss = criterion(output, output_signal.repeat(batch_size,1))
+        if smoothened and i > n_iter/3:
+            loss = loss + smooth_loss(input_tensor[...,1:,1:],input_tensor[...,:-1,:-1])\
+            + smooth_loss(input_tensor[...,1:,:-1],input_tensor[...,:-1,1:])\
+            + smooth_loss(input_tensor[...,1:,:],input_tensor[...,:-1,:])\
+            + smooth_loss(input_tensor[...,:,1:],input_tensor[...,:,:-1])
         loss.backward()
         input_tensor = normalizeZeroOne(input_tensor.detach()-input_tensor.grad)
     if reduction:
