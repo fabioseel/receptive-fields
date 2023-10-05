8f228c19e3914432299f1fea0a04e74f7a3fcc5f
Fabio Seel <fabio.seel@gmail.com>
2023-10-05 11:02:13 +0200

    feat: automatic calculation of inp / out size

--- src/activation_visualization.py
+++ src/activation_visualization.py
@@ -2,6 +2,8 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch
 import numpy as np
+from tqdm import tqdm
+import math
 
 def normalizeZeroOne(input):
     return (input - input.min()) / (input.max()-input.min())
@@ -22,18 +24,71 @@ def dataset_average(model: nn.Module, dataloader: DataLoader, desired_output = t
 
     return normalizeZeroOne(weighted_sum)
 
-def effective_receptive_field(model:nn.Module, output_signal:torch.tensor, input_size:torch.Size, n_iter:int=2048):
+def effective_receptive_field(model:nn.Module, output_signal:torch.tensor, n_batch:int=2048):
     model.eval()
-    input_tensor = torch.randn((n_iter, *input_size), requires_grad=True)
+    _out_channels, input_size = get_input_output_shape(model)
+    input_tensor = torch.randn((n_batch, *input_size), requires_grad=True)
     output = model(input_tensor)
     target_output = output * output_signal
     target_output.sum().backward()
     eff_rf = input_tensor.grad.sum(0)
     return normalizeZeroOne(eff_rf)
 
-def backprop_maximization(model:nn.Module, output_signal:torch.tensor, input_size:torch.Size, n_iter:int=2048, batch_size=16, reduction=True):
+def _activation_triggered_average(model:nn.Module, n_batch:int=2048):
+    model.eval()
+    _out_channels, input_size = get_input_output_shape(model)
+    input_tensor = torch.randn((n_batch, *input_size), requires_grad=False)
+    output = model(input_tensor)
+    output=output.squeeze()
+    input_tensor= input_tensor[:, None,:,:,:].expand(-1,output.shape[1],-1,-1,-1)
+
+    weights = output[:, :, None, None, None].expand(-1, -1, *input_size)
+    weight_sums = output.abs().sum(0)
+    weight_sums[weight_sums==0]=1
+    weighted = (weights.abs() * input_tensor).sum(0) / weight_sums[:,None,None,None]
+    return weighted.detach()/n_batch
+
+def activation_triggered_average(model:nn.Module, n_batch:int=2048, n_iter:int=1):
+    weighted=_activation_triggered_average(model, n_batch)
+    for _ in tqdm(range(n_iter-1), total=n_iter, initial=1):
+        weighted+= _activation_triggered_average(model, n_batch)
+    return weighted.detach()/n_iter
+
+def get_input_output_shape(model: nn.Sequential):
+    _first = 0
+    for layer in reversed(model):
+        _first+=1
+        if isinstance(layer, nn.Linear):
+            num_outputs = layer.out_features
+            in_channels = 1
+            in_size = layer.in_features
+            break
+        elif isinstance(layer, nn.Conv2d):
+            num_outputs = layer.out_channels
+            in_channels = layer.in_channels
+            in_size = layer.in_channels * layer.kernel_size[0]**2
+            break
+
+    for layer in reversed(model[:-_first]):
+        if isinstance(layer, nn.Linear):
+            in_channels = 1
+            in_size = layer.in_features
+        elif isinstance(layer, nn.Conv2d):
+            in_channels = layer.in_channels
+            in_size = math.sqrt(in_size/layer.out_channels)
+            in_size = (in_size - 1) * layer.stride[0] - 2*layer.padding[0] + layer.kernel_size[0]
+            in_size = in_size**2 * in_channels
+    
+    in_size = math.floor(math.sqrt(in_size/in_channels))
+    input_size = (in_channels, in_size, in_size)
+    return num_outputs, input_size
+
+
+def backprop_maximization(model:nn.Module, output_signal:torch.tensor, n_iter:int=2048, batch_size=16, reduction=True):
     model.eval()
     criterion = nn.CrossEntropyLoss()
+
+    num_outputs, input_size = get_input_output_shape(model)
 
     input_tensor = torch.randn((batch_size,*input_size), requires_grad=True)
     for i in range(n_iter):
