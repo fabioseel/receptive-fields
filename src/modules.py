a8d560348f725f9d8018de0d7cbde7d16fbb3323
Fabio Seel <fabio.seel@uni-tuebingen.de>
2023-11-13 17:39:47 +0100

    refactor: comp. optimization of GaborConv2D

--- src/modules.py
+++ src/modules.py
@@ -1,10 +1,8 @@
 import math
-from typing import Any
-
 import torch
 from torch.nn import Parameter
 from torch.nn.modules import Conv2d, Module
-
+from torch.nn.modules.utils import _pair
 
 class GaborConv2d(Module):
     def __init__(
@@ -25,20 +23,19 @@ class GaborConv2d(Module):
         """
         super().__init__()
 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _pair(padding) # TODO: implement padding_mode
+        self.dilation = _pair(dilation)
+        self.groups = groups
         self.is_calculated = False
 
-        self.conv_layer = Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding,
-            dilation,
-            groups,
-            bias,
-            padding_mode,
-        )
-        self.kernel_size = self.conv_layer.kernel_size
+        if bias:
+            self.bias = Parameter(torch.empty(out_channels), requires_grad=True)
+        else:
+            self.bias = Parameter(torch.zeros(out_channels), requires_grad=False)
 
         # small addition to avoid division by zero
         self.delta = 1e-3
@@ -74,39 +71,33 @@ class GaborConv2d(Module):
             [
                 torch.linspace(-self.x0 + 1, self.x0 + 0, self.kernel_size[0]),
                 torch.linspace(-self.y0 + 1, self.y0 + 0, self.kernel_size[1]),
-            ]
+            ],
+            indexing="ij"
         )
-        self.y = Parameter(self.y)
-        self.x = Parameter(self.x)
+        self.y = Parameter(self.y, requires_grad=False)
+        self.x = Parameter(self.x, requires_grad=False)
 
         self.weight = Parameter(
-            torch.empty(self.conv_layer.weight.shape, requires_grad=True),
-            requires_grad=True,
+            torch.empty((out_channels, in_channels//groups, self.kernel_size[0], self.kernel_size[1])),
+            requires_grad=False
         )
 
-        self.register_parameter("freq", self.freq)
-        self.register_parameter("theta", self.theta)
-        self.register_parameter("sigma", self.sigma)
-        self.register_parameter("psi", self.psi)
-        self.register_parameter("x_shape", self.x0)
-        self.register_parameter("y_shape", self.y0)
-        self.register_parameter("y_grid", self.y)
-        self.register_parameter("x_grid", self.x)
-        self.register_parameter("weight", self.weight)
+    def requires_grad(self):
+        return any(param.requires_grad for param in self.parameters())
 
     def forward(self, input_tensor):
-        if self.training:
+        if self.training and self.requires_grad():
             self.calculate_weights()
             self.is_calculated = False
-        if not self.training:
+        if not self.training or not self.requires_grad():
             if not self.is_calculated:
                 self.calculate_weights()
                 self.is_calculated = True
-        return self.conv_layer(input_tensor)
+        return torch.nn.functional.conv2d(input_tensor, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
 
-    def calculate_weights(self):
-        for i in range(self.conv_layer.out_channels):
-            for j in range(self.conv_layer.in_channels):
+    def calculate_weights2(self):
+        for i in range(self.weight.shape[0]):
+            for j in range(self.weight.shape[1]):
                 sigma = self.sigma[i, j].expand_as(self.y)
                 freq = self.freq[i, j].expand_as(self.y)
                 theta = self.theta[i, j].expand_as(self.y)
@@ -120,11 +111,20 @@ class GaborConv2d(Module):
                 )
                 g = g * torch.cos(freq * rotx + psi)
                 g = g / (2 * math.pi * sigma ** 2)
-                self.conv_layer.weight.data[i, j] = g
+                self.weight.data[i, j] = g
 
-    def _forward_unimplemented(self, *inputs: Any):
-        """
-        code checkers makes implement this method,
-        looks like error in PyTorch
-        """
-        raise NotImplementedError
+    def calculate_weights(self):
+        sigma = self.sigma[:,:,None,None].expand(-1,-1,*self.y.shape)
+        freq = self.freq[:,:,None,None].expand(-1,-1,*self.y.shape)
+        theta = self.theta[:,:,None,None].expand(-1,-1,*self.y.shape)
+        psi = self.psi[:,:,None,None].expand(-1,-1,*self.y.shape)
+
+        rotx = self.x * torch.cos(theta) + self.y * torch.sin(theta)
+        roty = -self.x * torch.sin(theta) + self.y * torch.cos(theta)
+
+        g = torch.exp(
+            -0.5 * ((rotx ** 2 + roty ** 2) / (sigma + self.delta) ** 2)
+        )
+        g = g * torch.cos(freq * rotx + psi)
+        g = g / (2 * math.pi * sigma ** 2)
+        self.weight.data = g
