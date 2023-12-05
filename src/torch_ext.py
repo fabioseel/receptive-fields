858ad9245da50e62346329b396c89a938df07e86
Fabio Seel <fabio.seel@uni-tuebingen.de>
2023-12-05 17:33:26 +0100

    feat: add weight regularizer, implement l1/2 reg

--- src/torch_ext.py
+++ src/torch_ext.py
@@ -6,12 +6,21 @@ class OutputHook(list):
     def __call__(self, module, input, output):
         self.append(output)
 
+def l1reg(x:torch.Tensor):
+    return torch.abs(x).sum()
+
+def l2reg(x:torch.Tensor):
+    return torch.pow(x, 2).sum()
+
 class ActivationRegularization():
     def __init__(self, module = torch.nn.Module | list, p=2.0, act_lambda=0.01):
         self._lambda = act_lambda
         if act_lambda > 0:
             self.hook = OutputHook()
-            self.p = p
+            if p == 1:
+                self.norm = l1reg
+            else:
+                self.norm = l2reg
             if isinstance(module, torch.nn.Module):
                 module.register_forward_hook(self.hook)
             else:
@@ -21,8 +30,22 @@ class ActivationRegularization():
     def penalty(self):
         penalty = 0.
         if self._lambda > 0:
-            for output in self.hook:
-                penalty += torch.norm(output, self.p)
-            penalty *= self._lambda
+            penalty = self._lambda * sum(self.norm(output) for output in self.hook)
             self.hook.clear()
+        return penalty
+
+class WeightRegularization():
+    def __init__(self, module = torch.nn.Module | list, p=2.0, weight_decay=0.01):
+        self._lambda = weight_decay
+        if self._lambda > 0:
+            if p == 1:
+                self.norm = l1reg
+            else:
+                self.norm = l2reg
+            self.params = module.parameters()
+
+    def penalty(self):
+        penalty = 0.
+        if self._lambda > 0:
+            penalty = self._lambda * sum(self.norm(param) for param in self.params)
         return penalty
