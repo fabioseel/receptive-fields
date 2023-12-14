676932dcf85c5462b9c6656db614f060f20fd57f
Fabio Seel <fabio.seel@uni-tuebingen.de>
2023-12-14 14:16:22 +0100

    chore: ipynb updates

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/alexnet_optim/alexnet_sgd_wdecay00001 cifar10 32 0.01 --optim=sgd --weight_decay=1e-4 --momentum=0.5 --early_stop=10 --save_hist
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/alexnet_optim/alexnet_sgd_wdecay0000001 cifar10 32 0.01 --optim=sgd --weight_decay=1e-6 --momentum=0.5 --early_stop=10 --save_hist
+python train.py ../models/lindsey_weight_reg_act/l2reg_leaky cifar10 32 0.01 --optim="msgdw" --weight_decay=2e-4 --weight_norm=2 --early_stop=10 --save_hist
 
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/lindsey_optim/lindseydefaultgrey cifar10 32 0.01 --optim=sgd --weight_decay=1e-3 --momentum=0.5 --early_stop=10 --save_hist
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/lindsey_optim/lindsey32 cifar10 32 0.01 --optim=sgd --weight_decay=1e-3 --momentum=0.5 --early_stop=10 --save_hist
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/lindsey_optim/lindsey32grey cifar10 32 0.01 --optim=sgd --weight_decay=1e-3 --momentum=0.5 --early_stop=10 --save_hist
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/lindsey_optim/color_ks3 cifar10 32 0.01 --optim=sgd --weight_decay=1e-3 --momentum=0.5 --early_stop=10 --save_hist
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/lindsey_optim/color_ks5 cifar10 32 0.01 --optim=sgd --weight_decay=1e-3 --momentum=0.5 --early_stop=10 --save_hist
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/lindsey_optim/lindsey32gelu cifar10 32 0.01 --optim=sgd --weight_decay=1e-3 --momentum=0.5 --early_stop=10 --save_hist
-CUDA_VISIBLE_DEVICES=0 python train.py ../models/lindsey_optim/lindseydefaultgelu cifar10 32 0.01 --optim=sgd --weight_decay=1e-3 --momentum=0.5 --early_stop=10 --save_hist
+python train.py ../models/lindsey_act/color_ks5_leaky cifar10 128 0.0001 --optim="sgd"
